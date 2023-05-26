b284b0253e78f5b0e252f6d4bbfe5848185f114a
Leonardo <leonardoar2@al.insper.edu.br>
2023-05-26 14:18:22 -0300

    .

--- App v.2/js/script.js
+++ App v.2/js/script.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-
+    botao_emergencia = document.getElementById("emergencia");
+    botao_emergencia.addEventListener("onclick", function(){
+        botao_emergencia.style.backgroundColor = "red";
+    })
     
 })
