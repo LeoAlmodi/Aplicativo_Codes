c7aec92e25952a53864c992da7395dfad309aa06
Leonardo <leonardoar2@al.insper.edu.br>
2023-05-31 08:35:57 -0300

    .

--- App v.2/js/script.js
+++ App v.2/js/script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    botao_emergencia = document.getElementById("emergencia");
-    botao_emergencia.addEventListener("mousedown", function(){
-        botao_emergencia.style.backgroundColor = "red"; 
-    })
+    // botao_emergencia = document.getElementById("emergencia");
+    // botao_emergencia.addEventListener("mousedown", function(){
+    //     botao_emergencia.style.backgroundColor = "red"; 
+    // })
+
+    
     
 
 })
