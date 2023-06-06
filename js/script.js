a466e33926b0eaa1d5b595f06ddfcd3d49fb8199
Vítor Nucci Sevá Pereira <vitornsp@al.insper.edu.br>
2023-06-06 15:49:41 -0300

    ....

--- js/script.js
+++ js/script.js
@@ -38,27 +38,11 @@ document.addEventListener("DOMContentLoaded", function(){
 
 // ------------------------------------------------------------------------------------------
 
-
-    // botao_emergencia = document.getElementById("emergencia");
-    // botao_emergencia.addEventListener("mousedown", function(){
-    //     botao_emergencia.style.backgroundColor = "red"; 
-    // })
-
-    // compartilhar1 = document.getElementById("comp1");
     card1_comp = document.getElementById("card1");
     compartilhar2 = document.getElementById("comp2");
     compartilhar3 = document.getElementById("comp3");
     compartilhar4 = document.getElementById("comp4");
-    // compartilhar1.addEventListener('click', function(){
-    //     if (compartilhar1.innerHTML != "Compartilhado"){
-    //         compartilhar1.innerHTML = "Compartilhado";
 
-
-    //     }
-    //     else {
-    //         compartilhar1.innerHTML = "Compartilhar";
-    //     }})
-    
     compartilhar2.addEventListener('click', function(){
         if (compartilhar2.innerHTML != "Compartilhado"){
             compartilhar2.innerHTML = "Compartilhado";
