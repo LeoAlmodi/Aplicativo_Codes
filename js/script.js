655e9f0232e0dd742ecbe18d28de43718ad2c3cb
Leonardo <leonardoar2@al.insper.edu.br>
2023-06-06 11:34:43 -0300

    .

--- js/script.js
+++ js/script.js
@@ -5,17 +5,50 @@ document.addEventListener("DOMContentLoaded", function(){
     //     botao_emergencia.style.backgroundColor = "red"; 
     // })
 
-    compartilhar1 = document.getElementById("comp1");
+    // compartilhar1 = document.getElementById("comp1");
+    card1_comp = document.getElementById("card1");
     compartilhar2 = document.getElementById("comp2");
     compartilhar3 = document.getElementById("comp3");
     compartilhar4 = document.getElementById("comp4");
-    compartilhar1.addEventListener('click', function(){
-        if (compartilhar1.innerHTML != "Compartilhado"){
-            compartilhar1.innerHTML = "Compartilhado";
-            compartilhar.style.backgroundColor = 'blue';
+    // compartilhar1.addEventListener('click', function(){
+    //     if (compartilhar1.innerHTML != "Compartilhado"){
+    //         compartilhar1.innerHTML = "Compartilhado";
+
+
+    //     }
+    //     else {
+    //         compartilhar1.innerHTML = "Compartilhar";
+    //     }})
+    
+    compartilhar2.addEventListener('click', function(){
+        if (compartilhar2.innerHTML != "Compartilhado"){
+            compartilhar2.innerHTML = "Compartilhado";
+            
 
         }
         else {
-            compartilhar1.innerHTML = "Compartilhar";
+            compartilhar2.innerHTML = "Compartilhar";
         }})
+
+    compartilhar3.addEventListener('click', function(){
+        if (compartilhar3.innerHTML != "Compartilhado"){
+            compartilhar3.innerHTML = "Compartilhado";
+            
+
+        }
+        else {
+            compartilhar3.innerHTML = "Compartilhar";
+        }})
+
+        compartilhar4.addEventListener('click', function(){
+        if (compartilhar4.innerHTML != "Compartilhado"){
+            compartilhar4.innerHTML = "Compartilhado";
+            
+
+        }
+        else {
+            compartilhar4.innerHTML = "Compartilhar";
+        }})
+
+
 })
