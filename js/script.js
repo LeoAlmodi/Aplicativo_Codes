872e0b18e50102d83ce76272cb482eafcfeb07f4
Vítor Nucci Sevá Pereira <vitornsp@al.insper.edu.br>
2023-06-06 13:17:37 -0300

    java buscar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+    // ------------------------------------------------------------------------------------------
+    //                          BUSCA DE MÉDICOS
+    
+    const userCardTemplate = document.querySelector("[data-user-template]")
+    const userCardContainer = document.querySelector("[data-user-cards-container]")
+    const searchInput = document.querySelector("[data-search]")
+
+    let users = []
+
+    searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+    users.forEach(user => {
+        const isVisible =
+        user.name.toLowerCase().includes(value) ||
+        user.email.toLowerCase().includes(value)
+        user.element.classList.toggle("hide", !isVisible)
+    })
+    })
+
+    fetch("json/users.json")
+    .then(response => response.json())
+    .then(data => {
+        users = data.map(user => {
+        const card = userCardTemplate.content.cloneNode(true).children[0]
+        const header = card.querySelector("[data-header]")
+        const body = card.querySelector("[data-body]")
+        header.textContent = user.name
+        body.textContent = user.email
+        userCardContainer.append(card)
+        return { name: user.name, email: user.email, element: card }
+        })
+    })
+
+// ------------------------------------------------------------------------------------------
+
+
     // botao_emergencia = document.getElementById("emergencia");
     // botao_emergencia.addEventListener("mousedown", function(){
     //     botao_emergencia.style.backgroundColor = "red"; 
@@ -50,36 +86,4 @@ document.addEventListener("DOMContentLoaded", function(){
             compartilhar4.innerHTML = "Compartilhar";
         }})
 
-
-// ---------------------------------------------------------------------------------------------
-
-    const userCardTemplate = document.querySelector("[data-user-template]")
-    const userCardContainer = document.querySelector("[data-user-cards-container]")
-    const searchInput = document.querySelector("[data-search]")
-
-    let users = []
-
-    searchInput.addEventListener("input", e => {
-    const value = e.target.value.toLowerCase()
-    users.forEach(user => {
-        const isVisible =
-        user.name.toLowerCase().includes(value) ||
-        user.email.toLowerCase().includes(value)
-        user.element.classList.toggle("hide", !isVisible)
-    })
-    })
-
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res => res.json())
-    .then(data => {
-        users = data.map(user => {
-        const card = userCardTemplate.content.cloneNode(true).children[0]
-        const header = card.querySelector("[data-header]")
-        const body = card.querySelector("[data-body]")
-        header.textContent = user.name
-        body.textContent = user.email
-        userCardContainer.append(card)
-        return { name: user.name, email: user.email, element: card }
-        })
-    })
 })
