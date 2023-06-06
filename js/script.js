2dce968e2e17f4f4e4de544c05d355ac24f1ceb0
Leonardo <leonardoar2@al.insper.edu.br>
2023-06-06 11:07:52 -0300

    ..

--- js/script.js
+++ js/script.js
@@ -5,5 +5,17 @@ document.addEventListener("DOMContentLoaded", function(){
     //     botao_emergencia.style.backgroundColor = "red"; 
     // })
 
+    compartilhar1 = document.getElementById("comp1");
+    compartilhar2 = document.getElementById("comp2");
+    compartilhar3 = document.getElementById("comp3");
+    compartilhar4 = document.getElementById("comp4");
+    compartilhar1.addEventListener('click', function(){
+        if (compartilhar1.innerHTML != "Compartilhado"){
+            compartilhar1.innerHTML = "Compartilhado";
+            compartilhar.style.backgroundColor = 'blue';
 
+        }
+        else {
+            compartilhar1.innerHTML = "Compartilhar";
+        }})
 })
