5012a03d9744c6cb17675e00beeac6d6754d6942
Vítor Nucci Sevá Pereira <vitornsp@al.insper.edu.br>
2023-06-06 11:35:21 -0300

    ..

--- js/calendario.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-
-    // botao_emergencia = document.getElementById("emergencia");
-    // botao_emergencia.addEventListener("mousedown", function(){
-    //     botao_emergencia.style.backgroundColor = "red"; 
-    // })
-
-
-})
